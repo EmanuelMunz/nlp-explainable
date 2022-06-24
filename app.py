b940b9982f62afc56632125802185fce5356684e
EmanuelMunz <88891111+EmanuelMunz@users.noreply.github.com>
2022-06-24 16:59:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-
 import streamlit as st
 from main import reviews_test,df_show, exp_DT, exp_SVM, exp_NB, exp_LR, exp_GB, classifier_DT,classifier_GB,classifier_LR,classifier_NB,classifier_SVM, y_test, test_vectors
+from design import color_df
 from ConfusionMatrix import createCM
 from WordsImportance import make_importantWordsPlot
 import streamlit.components.v1 as components
@@ -15,6 +15,19 @@ def main():
     st.set_option('deprecation.showPyplotGlobalUse', False)
     st.set_page_config(layout="wide")
     st.title('NLP - make it explainable')
+    st.subheader('Sentiment Analysis')
+
+    #explain field
+    expander = st.expander("See explanation")
+    expander.write("""
+         This dashboard makes 5 different sentiment classifier explainable. They are trained on amazon movie reviews. This dashboard 
+         uses LIME, a framework for XAI.
+     """)
+
+    #Seperator line
+    st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
+
+
 
     #dict
     dictionary = {'negative': 0, 'neutral': 1, 'positive': 2}
@@ -24,11 +37,33 @@ def main():
 
     # Data Frame
     st.subheader('Amazon Movie Reviews')
+
+    #explain field
+    expander = st.expander("See explanation")
+    expander.write("""
+         The table below shows a choosen number of amazon movie reviews from the test data set.
+        It includes additional information whether the classifier predicted the underlying review correctly. By clicking on the
+        headers of the columns you can filter the reviews.
+     """)
+
     nb_rows = st.slider('Number of reviews to display', min_value=2, max_value=600, value=5, step=None,
                         format=None)
-    st.dataframe(df_show.head(nb_rows))
+    #st.dataframe(df_show.head(nb_rows))
+    st.dataframe(df_show.head(nb_rows).style.applymap(color_df, subset=['SVM_right','LR_right','GB_right','DT_right','NB_right']))
+
+    #Seperator line
+    st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
 
     if st.sidebar.checkbox("Single review analysis"):
+        st.subheader('Single Review Analysis')
+
+        # explain field
+        expander = st.expander("See explanation")
+        expander.write("""
+             The chart above shows some numbers I picked for you.
+             I rolled actual dice for these, so they're *guaranteed* to
+             be random.
+         """)
 
         revindex = st.sidebar.number_input('Insert index of checked review', min_value = 0, max_value = 599, value =  0, step =  1 )
 
@@ -43,8 +78,14 @@ def main():
 
         #st.write('Text of the review: '+ df_show['body'][revindex])
         textbody = (df_show['body'][revindex])
-        st.subheader('Text of review '+str(revindex))
-        st.text_area(label='',value=textbody,disabled=True)
+        #st.subheader('Text of review '+str(revindex))
+        #st.text_area(label='',value=textbody,disabled=True)
+
+        # explain field
+        expander = st.expander("Show review text")
+        expander.write(textbody)
+
+
         reviews_subset = reviews_test[(["body"])]
         #reviews_subset = reviews_subset(revindex)
         #st.dataframe(reviews_subset.head())
@@ -69,8 +110,19 @@ def main():
             st.subheader('GB')
             components.html(exp_GB[revindex].as_html(),width = 1800, height = 250)
 
+        #Seperator line
+        st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
+
     if st.sidebar.checkbox("Confusion Matrix"):
         st.subheader('Confusion Matrix')
+
+        # explain field
+        expander = st.expander("See explanation")
+        expander.write("""
+             The confusion matrix shows the accuracy of the classifier on the test set. It is useable to identify in what scenarios 
+             the classifier perform well or bad.
+         """)
+
         confmatr = st.sidebar.multiselect("Choose the classifiers", ["DT", "SVM", "NB", "LR", "GB"], key=2)
         col1, col2 = st.columns(2)
         # Wie viele clf sind ausgewählt in Dropdown?
@@ -94,21 +146,21 @@ def main():
                 clf_list.remove('DT')
             elif "SVM" in confmatr:
                 #dictionary2[array[0]].subheader('SupportVectorMachine')
-                dictionary2[array[0]].pyplot(createCM(classifier=classifier_SVM, title="Confusion Matrix DT", y_test=y_test,
+                dictionary2[array[0]].pyplot(createCM(classifier=classifier_SVM, title="Confusion Matrix SVM", y_test=y_test,
                                      test_vectors=test_vectors))
                 # cut first index and drop clf
                 array.pop(0)
                 clf_list.remove('SVM')
             elif "NB" in confmatr:
                 #dictionary2[array[0]].subheader('NaiveBayes')
-                dictionary2[array[0]].pyplot(createCM(classifier=classifier_NB, title="Confusion Matrix DT", y_test=y_test,
+                dictionary2[array[0]].pyplot(createCM(classifier=classifier_NB, title="Confusion Matrix NB", y_test=y_test,
                                      test_vectors=test_vectors))
                 # cut first index and drop clf
                 array.pop(0)
                 clf_list.remove('NB')
             elif "LR" in confmatr:
                 #dictionary2[array[0]].subheader('LogisticRegression')
-                dictionary2[array[0]].pyplot(createCM(classifier=classifier_LR, title="Confusion Matrix DT", y_test=y_test,
+                dictionary2[array[0]].pyplot(createCM(classifier=classifier_LR, title="Confusion Matrix LR", y_test=y_test,
                                      test_vectors=test_vectors))
                 # cut first index and drop clf
                 array.pop(0)
@@ -116,18 +168,28 @@ def main():
 
             elif "GB" in confmatr:
                 #dictionary2[array[0]].subheader('GradientBoosting')
-                dictionary2[array[0]].pyplot(createCM(classifier=classifier_GB, title="Confusion Matrix DT", y_test=y_test,
+                dictionary2[array[0]].pyplot(createCM(classifier=classifier_GB, title="Confusion Matrix GB", y_test=y_test,
                                      test_vectors=test_vectors))
                 # cut first index and drop clf
                 array.pop(0)
                 clf_list.remove('GB')
+        #Seperator line
+        st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
 
     if st.sidebar.checkbox("Multiple review analysis"):
         st.subheader('Multiple Review Analysis')
+
+        # explain field
+        expander = st.expander("See explanation")
+        expander.write("""
+             The multiple review analysis shows the most important words on each label and classifier. They are ordered from highest to lower  
+             impact. The number in the bar shows how often the word influenced the sentiment prediction based on the test set.
+         """)
+
         selmulclf = st.sidebar.multiselect("Choose the classifiers", ["DT", "SVM", "NB", "LR", "GB"], key=3)
-        amountWords_show = st.slider('Number of words to display', min_value=1, max_value=10, value=5, step=None,
+        amountWords_show = st.slider('Number of words to display', min_value=1, max_value=20, value=5, step=None,
                             format=None)
-        label = st.radio("Explaination for label", ('negative', 'neutral', 'positive'))
+        label = st.radio("Explaination of label", ('negative', 'neutral', 'positive'))
         st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)#show radio as row
         col1, col2 = st.columns(2)
 
@@ -175,7 +237,9 @@ def main():
                 array.pop(0)
                 clf_list.remove('GB')
 
-
+        # Seperator line
+        st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#333;" /> """,
+                    unsafe_allow_html=True)
         #
         #     beeswarm_plot, ax = plt.subplots()
         #     ax = shap.plots.beeswarm(shap_values_multiple[:, :, 1], max_display=100)
@@ -191,5 +255,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-    ##########################################################
